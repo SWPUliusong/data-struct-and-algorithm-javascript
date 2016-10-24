64313a60f6305fd4dfd28483f475566f489a2a8e
SWPUliusong <1418441382@qq.com>
2016-10-24 16:45:38 +0800

    update

--- algorithm/test.js
+++ algorithm/test.js
@@ -11,4 +11,4 @@ deep(algo.select([5,2,3,4,1]), [1,2,3,4,5], 'select: ' + algo.select([5,2,3,4,1]
 deep(algo.insert([5,2,3,4,1]), [1,2,3,4,5], 'insert: ' + algo.insert([5,2,3,4,1]))
 deep(algo.mergeRec([5,2,3,4,1]), [1,2,3,4,5], 'mergeRec: ' + algo.mergeRec([5,2,3,4,1]))
 deep(algo.quick([5,2,3,4,1]), [1,2,3,4,5], 'quick: ' + algo.quick([5,2,3,4,1]))
-deep(algo.bucket([5,2,3,2,1]), [1,2,4,3,5], 'bucket: ' + algo.bucket([5,2,3,2,1]))
+deep(algo.bucket([5,2,3,2,1]), [1,2,2,3,5], 'bucket: ' + algo.bucket([5,2,3,2,1]))
